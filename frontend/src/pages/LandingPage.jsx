989d1626e5132357c8d9533f2aaddc71a05a175f
Sukhleen Kaur <skau811@aucklanduni.ac.nz>
2025-06-24 22:52:19 +1200

    feat: create PublicationsContainer component to organize and display recent publications in a modular way

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -8,6 +8,7 @@ import useCustomCentering from "../hooks/useCustomCentering";
 import { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import Carousel from "../components/Carousel";
+import PublicationContainer from "./PublicationsContainer.jsx";
 
 function LandingPage() {
   const marginRef = useRef();
@@ -53,7 +54,7 @@ function LandingPage() {
         <div className="w-screen h-auto bg-[url(/gradientImage.png)] bg-cover bg-no-repeat border-2">
           {/* for the gradient background */}
 
-          <div className="flex flex-col justify-between items-center xl:my-8 xl:py-0 py-8 px-1.5 gap-1.5 xl:max-w-64.5 mx-auto border-2 ">
+          <div className="flex flex-col justify-between items-center xl:my-8 xl:py-0 py-8 mx-1.5 gap-1.5 xl:max-w-64.5 xl:mx-auto border-2 ">
             <p className="heading3">Our work centers on three key themes</p>
 
             <div className="flex flex-col xl:flex-row items-center justify-between gap-1.5 ">
@@ -87,7 +88,7 @@ function LandingPage() {
             <Carousel movementAmount="480" />
           </section>
 
-          <p className="heading3 xl:max-w-64.5 my-8 px-1.5 mx-auto border-2">
+          <p className="heading3 xl:max-w-64.5 my-8 mx-1.5 xl:mx-auto border-2">
             Our research explores how reality itself - both physical and digital
             - can be leveraged to understand cognitive, behavioral, and
             emotional states, assist individuals in their daily lives, and
@@ -97,22 +98,24 @@ function LandingPage() {
             their full potential.
           </p>
 
-          <section className="flex flex-col bg-background-white xl:justify-items-start items-start w-screen ">
+          {/* <section className="flex flex-col bg-background-white xl:justify-items-start items-start w-screen ">
             <div className="flex flex-col justify-between items-start xl:items-center py-5 gap-4 xl:w-64.5 mx-auto border-2 bg-basewhite">
               <h1 className="heading1 self-start ">Recent Publications </h1>
 
               <div className="flex xl:flex-col flex-row flex-wrap  xl:justify-between  items-start xl:items-center xl:gap-2 gap-x-1.5 gap-y-2.5 ">
-                {/* will use a mapping function to return the latest publications once backend is ready! */}
+               
                 <PublicationListItem />
                 <PublicationListItem />
                 <PublicationListItem />
                 <PublicationListItem />
                 <PublicationListItem />
               </div>
-              {/* this needs to changed to a Link component */}
+              
               <p className="labelBold">view all publications</p>
             </div>
-          </section>
+          </section> */}
+
+          <PublicationContainer/>
 
           <section className="flex flex-col mx-auto py-5 px-1.5 xl:max-w-64.5 gap-4">
             <h1 className="heading1">News</h1>
---
+++ frontend/src/pages/PublicationsContainer.jsx
@@ -0,0 +1,25 @@
+import PublicationListItem from "../components/PublicationListItem";
+
+function PublicationContainer() {
+  return (
+    <>
+      {" "}
+      <section className="flex flex-col bg-background-white xl:justify-items-start items-start w-screen px-1.5">
+        <div className="flex flex-col justify-between items-start xl:items-center  py-5 gap-4 w-full xl:max-w-[1032px]  mx-auto">
+          <h1 className="heading1 self-start">Recent Publications</h1>
+
+          <div className="xl:flex xl:flex-col xl:items-center xl:gap-2 grid w-full gap-x-1.5 gap-y-2.5 grid-cols-[repeat(auto-fit,minmax(272px,1fr))]">
+            <PublicationListItem />
+            <PublicationListItem />
+            <PublicationListItem />
+            <PublicationListItem />
+          </div>
+
+          <p className="labelBold">view all publications</p>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default PublicationContainer;
