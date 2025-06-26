7777353f4411e82d7bfc5ceadd7bd8b7d21737d1
Sukhleen Kaur <skau811@aucklanduni.ac.nz>
2025-06-26 21:46:29 +1200

    feat: add modal functionality to Navbar for join inquiries

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import close from "../assets/icons/close.svg";
 import menu from "../assets/icons/menu.svg";
 import useScrollBeyondVisual from "../hooks/useScrollBeyondVisual";
 import useScrollDirection from "../hooks/useScrollDirection";
+import Modal from "./modals/Modal";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,11 +12,23 @@ function Navbar() {
     setIsOpen(!isOpen);
   };
 
+  const [modalType, setModalType] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenJoinModal = () => {
+    setIsModalOpen(true);
+    setModalType("join");
+  };
+
+  const handleOpenContactModal = () => {
+    setIsModalOpen(true);
+    setModalType("contact");
+  };
+
   // Change this threshold value (in pixels) according to the design specification, current placeholder image height is 800px
   const isScrolledBeyondVisual = useScrollBeyondVisual(800);
 
   const scrollDirection = useScrollDirection();
-
 
   let bgClass = "";
   bgClass = "bg-transparent";
@@ -28,15 +41,13 @@ function Navbar() {
     bgClass = "bg-transparent";
   }
 
-
-
-
   return (
     <>
+      {/* <Modal /> */}
       <nav
-        className={`label fixed top-0 left-1/2 transform -translate-x-1/2 z-50 bg-black flex flex-col xl:flex-row justify-between xl:gap-3 text-baseWhite  xl:max-w-75.5 w-full mx-auto  ${
-          bgClass
-        } ${scrollDirection === "down" ? "hidden" : "block"} ${!isOpen ? "min-h-fit" : "min-h-screen"}`}
+        className={`label fixed top-0 left-1/2 transform -translate-x-1/2 z-50 bg-black flex flex-col xl:flex-row justify-between xl:gap-3 text-baseWhite  xl:max-w-75.5 w-full mx-auto  ${bgClass} ${
+          scrollDirection === "down" ? "hidden" : "block"
+        } ${!isOpen ? "min-h-fit" : "min-h-screen"}`}
       >
         <ul className="flex flex-row justify-between items-end flex-none xl:flex-row xl:justify-between   py-1 px-1.5 xl:items-center">
           <li className="w-3 cursor-pointer">
@@ -59,9 +70,30 @@ function Navbar() {
           <li>people</li>
           <ul className="xl:flex-row xl:justify-between xl:gap-3 gap-1.5 flex flex-col">
             <li>
-              <button className="buttonPrimary bg-baseWhite xl:bg-background-black text-baseBlack xl:text-baseWhite">
+              <button
+                className="buttonPrimary bg-baseWhite xl:bg-background-black text-baseBlack xl:text-baseWhite"
+                onClick={() => handleOpenJoinModal()}
+              >
                 join us
               </button>
+              {isModalOpen && (
+                <Modal onClose={() => setIsModalOpen(false)} type={modalType}>
+                  <h1 className="heading1">
+                      Looking for opportunities to join us?
+                    </h1>
+                    <p className="body">
+                      As a new lab, we’re open to conversations and
+                      collaborations in many forms. Feel free to explore our
+                      projects and publications, if our work at the Inclusive
+                      Reality Lab resonates with your interests, don’t hesitate
+                      to reach out at inclusiverealitylab@gmail.com. Someone
+                      from our team or our director will be happy to get back to
+                      you soon.
+                    </p>
+                    
+                  
+                </Modal>
+              )}
             </li>
             <li>
               <button className="buttonPrimary  text-baseWhite  xl:text-baseBlack border-white xl:border-baseBlack">
