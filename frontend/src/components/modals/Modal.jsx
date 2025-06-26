a175d08904d3dd17b4353e2e4c800a2c87f116dd
Sukhleen Kaur <skau811@aucklanduni.ac.nz>
2025-06-26 23:21:43 +1200

    fix: adjust modal dimensions and spacing for improved layout

--- frontend/src/components/modals/Modal.jsx
+++ frontend/src/components/modals/Modal.jsx
@@ -1,15 +1,16 @@
 import closeBlack from "../../assets/icons/closeBlack.svg";
 import ReactDOM from "react-dom";
 
-function Modal({ children, onClose,type }) {
+function Modal({ children, onClose, type }) {
   return ReactDOM.createPortal(
     <>
-      <div className={`fixed top-0 right-0 w-full xl:max-w-67.75 h-screen z-100  ${type == "join" ? "bg-background-secondary" : "bg-background-tertiary"}`}>
-        <div className="flex flex-col items-center gap-5 px-2.5 py-2.5"><button className="align self-start" onClick={onClose}><img src={closeBlack}/></button>
-        <div className="w-32 flex flex-col items-center gap-2">{children}</div>
+      <div className={`fixed top-0 right-0 w-full xl:max-w-68 h-screen z-100  ${type == "join" ? "bg-background-secondary" : "bg-background-tertiary"}`}>
+        <div className="flex flex-col items-center gap-3"><button className="align self-start mt-2.5 ml-1.5" onClick={onClose}><img src={closeBlack}/></button>
+        <div className="w-33 flex flex-col items-center ">{children}</div>
         </div>
         
       </div>
+     
     </>,
     document.getElementById("modal-root")
   );
