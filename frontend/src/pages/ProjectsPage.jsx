c809cc246aa2edc80aa93a5d41312bf2eeb31447
Sukhleen Kaur <skau811@aucklanduni.ac.nz>
2025-07-02 15:08:12 +1200

    feat: add ProjectsContainer component for improved reusability for showing the project cards

--- frontend/src/pages/ProjectsPage.jsx
+++ frontend/src/pages/ProjectsPage.jsx
@@ -1,5 +1,6 @@
 import ProjectCard from "../components/ProjectCard";
 import projects from "../sampleData/projects";
+import ProjectsContainer from "../components/ProjectsContainer";
 
 function ProjectsPage() {
   
@@ -9,21 +10,9 @@ function ProjectsPage() {
         <div className="border-2 border-amber-500 px-1.5 xl:px-0 flex flex-col justify-between items-start  py-5 gap-4 w-full xl:max-w-[1032px]  xl:mx-auto">
           <h1 className="heading1 self-start">Projects</h1>
           <button className="heading4 mt-1">Current Projects</button>
-          <div className="grid gap-1.5 w-full grid-cols-[repeat(auto-fit,minmax(256px,1fr))] xl:grid-cols-[repeat(auto-fit,minmax(400px,1fr))]">
-            {projects
-              .filter((project) => project.current === true)
-              .map((project) => (
-                <ProjectCard key={project.id} project={project} />
-              ))}
-          </div>
+         <ProjectsContainer projects={projects.filter((project) => project.current === true)}/>
           <button className="heading4 mt-1">Past Projects</button>
-          <div className="grid gap-1.5 w-full grid-cols-[repeat(auto-fit,minmax(256px,1fr))] xl:grid-cols-[repeat(auto-fit,minmax(400px,1fr))]">
-            {projects
-              .filter((project) => project.current === false)
-              .map((project) => (
-                <ProjectCard key={project.id} project={project} />
-              ))}
-          </div>
+          <ProjectsContainer projects={projects.filter((project) => project.current === false)}/>
         </div>
       </div>
     </>
