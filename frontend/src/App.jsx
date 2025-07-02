9671b4b4bb5c58d1e3c424a4b8d6b17c053ec33a
Sukhleen Kaur <skau811@aucklanduni.ac.nz>
2025-07-02 14:39:39 +1200

    feat: add route in App to the projects page and implement the projects page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import ProjectsPage from "./pages/ProjectsPage";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
 
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
         </Routes>
 
         
