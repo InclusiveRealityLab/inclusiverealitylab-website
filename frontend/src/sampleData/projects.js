ba7e5edd0a2e344cbe5e9a3862ed4758ef705730
Sukhleen Kaur <skau811@aucklanduni.ac.nz>
2025-07-02 14:36:52 +1200

    feat: add a sample data folder for data to help frontend testing

---
+++ frontend/src/sampleData/projects.js
@@ -0,0 +1,12 @@
+const projects = [
+  { id: 1, title: "Project one sample name", current: true },
+  { id: 2, title: "Project two sample name", current: true },
+  { id: 3, title: "Project three sample name", current: true },
+  { id: 4, title: "Project four sample name", current: false },
+  { id: 5, title: "Project one sample name", current: false },
+  { id: 6, title: "Project two sample name", current: false },
+  { id: 7, title: "Project three sample name", current: false },
+  { id: 8, title: "Project four sample name", current: false },
+];
+
+export default projects;
