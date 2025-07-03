220f70dc69d508ff1ce5b211a6967ee7881c242b
Sukhleen Kaur <skau811@aucklanduni.ac.nz>
2025-07-03 17:28:22 +1200

    fix: logic for grouping publications by year (removing duplicates), integrate the collapsible container into the publications page

--- frontend/src/pages/PublicationsPage.jsx
+++ frontend/src/pages/PublicationsPage.jsx
@@ -1,3 +1,4 @@
+import CollapsiblePubContainer from "../components/CollapsiblePubContainer";
 import publications from "../sampleData/publications";
 
 function PublicationsPage() {
@@ -7,16 +8,14 @@ function PublicationsPage() {
         <div className="border-2 border-amber-500 px-1.5 xl:px-0 flex flex-col justify-between items-start  py-5 gap-4 w-full xl:max-w-[1032px]  xl:mx-auto">
           {" "}
           <h1 className="heading1 self-start">Publications</h1>
-          {console.log(
-            publications
-              .map((pub) => pub.year)
-              .map((year) => {
-                return {
-                  year,
-                  items: publications.filter((pub) => pub.year === year),
-                };
-              })
-          )}
+          {(
+              [...new Set (publications
+                .map((pub) => pub.year))]
+                .map((year) => {
+                  const items = publications.filter((pub) => pub.year === year);
+                  return <CollapsiblePubContainer key={year} data={{ year, items }} />;
+                })
+            )}
           ;
         </div>
       </div>
