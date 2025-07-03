51701f9d1ac7057cfc3b05bb2d8c97c9397b3349
Sukhleen Kaur <skau811@aucklanduni.ac.nz>
2025-07-03 17:36:33 +1200

    refactor: create a publication section wrapper component for resuability

--- frontend/src/pages/PublicationsPage.jsx
+++ frontend/src/pages/PublicationsPage.jsx
@@ -1,24 +1,17 @@
 import CollapsiblePubContainer from "../components/CollapsiblePubContainer";
+import PublicationSectionWrapper from "../components/wrappers/PublicationSectionWrapper";
 import publications from "../sampleData/publications";
 
 function PublicationsPage() {
   return (
     <>
-      <div className="w-screen bg-background-white">
-        <div className="border-2 border-amber-500 px-1.5 xl:px-0 flex flex-col justify-between items-start  py-5 gap-4 w-full xl:max-w-[1032px]  xl:mx-auto">
-          {" "}
-          <h1 className="heading1 self-start">Publications</h1>
-          {(
-              [...new Set (publications
-                .map((pub) => pub.year))]
-                .map((year) => {
-                  const items = publications.filter((pub) => pub.year === year);
-                  return <CollapsiblePubContainer key={year} data={{ year, items }} />;
-                })
-            )}
-          ;
-        </div>
-      </div>
+      <PublicationSectionWrapper headingContent="Publications">
+        <h1 className="heading1 self-start">Publications</h1>
+        {[...new Set(publications.map((pub) => pub.year))].map((year) => {
+          const items = publications.filter((pub) => pub.year === year);
+          return <CollapsiblePubContainer key={year} data={{ year, items }} />;
+        })}
+      </PublicationSectionWrapper>
     </>
   );
 }
