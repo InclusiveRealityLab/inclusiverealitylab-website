c71794e9bfcd2f6c3e38e8d517292491d887db9d
Sukhleen Kaur <skau811@aucklanduni.ac.nz>
2025-07-09 13:07:59 +1200

    feat: expand the first collapsible container by default, auto-expand the following yearly collapsible containers as they become visible in viewport (loading not applied yet)

--- frontend/src/pages/PublicationsPage.jsx
+++ frontend/src/pages/PublicationsPage.jsx
@@ -2,7 +2,7 @@ import CollapsiblePubContainer from "../components/CollapsiblePubContainer";
 import PublicationsContainer from "../components/PublicationsContainer";
 import PublicationSectionWrapper from "../components/wrappers/PublicationSectionWrapper";
 import publications from "../sampleData/publications";
-import { useState } from "react";
+import { useState,useEffect,useRef } from "react";
 
 function PublicationsPage() {
   const uniqueYears = [...new Set(publications.map((pub) => pub.year))];
@@ -14,6 +14,47 @@ function PublicationsPage() {
   });
 
   const [yearsOpen, setYearsOpen] = useState([yearsSorted[0]]);
+  const containerRefs = useRef({});
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const updatedOpenYears = new Set([yearsSorted[0]]);
+        entries.forEach((entry) => {
+          const year = entry.target.getAttribute("data-year");
+          if (!year) {
+            return;
+          }
+          if (entry.isIntersecting) {
+            updatedOpenYears.add(parseInt(year));
+          }
+        });
+
+        const newOpen = Array.from(updatedOpenYears);
+        setYearsOpen((prev) => {
+          const same =
+            prev.length === newOpen.length &&
+            prev.every((v) => newOpen.includes(v));
+          return same ? prev : newOpen;
+        });
+      },
+      {
+        threshold: 0.,
+        rootMargin: "0px 0px -100px 0px",
+      }
+    );
+    yearsSorted.forEach((year) => {
+      const ref = containerRefs.current[year];
+      if (ref) {
+        observer.observe(ref);
+      }
+    });
+    return () => {
+      Object.values(containerRefs.current).forEach((ref) => {
+        if (ref) observer.unobserve(ref);
+      });
+    };
+  }, [yearsSorted]);
 
   return (
     <div className="mt-4">
@@ -23,6 +64,7 @@ function PublicationsPage() {
             key={year}
             data={{ year, items }}
             isExpandedContainer={yearsOpen.includes(year)}
+            ref={(el) => (containerRefs.current[year] = el)}
           >
             <PublicationsContainer publications={items} year={year} />
           </CollapsiblePubContainer>
