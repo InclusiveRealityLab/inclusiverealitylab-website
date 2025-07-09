a59b84326c8214609e141f6b9be68301717923a4
Sukhleen Kaur <skau811@aucklanduni.ac.nz>
2025-07-09 13:00:22 +1200

    refactor: sorting and grouping logic in publications page for better readibility, prepare for expansion feature

--- frontend/src/components/PublicationsContainer.jsx
+++ frontend/src/components/PublicationsContainer.jsx
@@ -3,16 +3,12 @@ import PublicationListItem from "./PublicationListItem";
 function PublicationsContainer({ publications }) {
   return (
     <>
-     
-        <div className="xl:flex xl:flex-col xl:items-start xl:gap-2 grid gap-x-1.5 gap-y-2.5 w-full grid-cols-[repeat(auto-fit,minmax(272px,1fr))] ">
-          {publications &&
-            publications.map((pub) => (
-              <PublicationListItem key={pub.id} publication={pub} />
-            ))}
-          
-        </div>
-
-       
+      <div className="xl:flex xl:flex-col xl:items-start xl:gap-2 grid gap-x-1.5 gap-y-2.5 w-full grid-cols-[repeat(auto-fit,minmax(272px,1fr))] ">
+        {publications &&
+          publications.map((pub) => (
+            <PublicationListItem key={pub.id} publication={pub} />
+          ))}
+      </div>
     </>
   );
 }
--- frontend/src/pages/PublicationsPage.jsx
+++ frontend/src/pages/PublicationsPage.jsx
@@ -2,15 +2,31 @@ import CollapsiblePubContainer from "../components/CollapsiblePubContainer";
 import PublicationsContainer from "../components/PublicationsContainer";
 import PublicationSectionWrapper from "../components/wrappers/PublicationSectionWrapper";
 import publications from "../sampleData/publications";
+import { useState } from "react";
 
 function PublicationsPage() {
+  const uniqueYears = [...new Set(publications.map((pub) => pub.year))];
+  const yearsSorted = [...uniqueYears].sort((a, b) => b - a);
+
+  const yearlyPublications = [...yearsSorted].map((year) => {
+    const items = publications.filter((pub) => pub.year === year);
+    return { year, items };
+  });
+
+  const [yearsOpen, setYearsOpen] = useState([yearsSorted[0]]);
+
   return (
     <div className="mt-4">
       <PublicationSectionWrapper headingContent="Publications">
-        {[...new Set(publications.map((pub) => pub.year))].map((year) => {
-          const items = publications.filter((pub) => pub.year === year);
-          return <><CollapsiblePubContainer key={year} data={{ year, items }} ><PublicationsContainer publications={items}/></CollapsiblePubContainer></>;
-        })}
+        {yearlyPublications.map(({ year, items }, index) => (
+          <CollapsiblePubContainer
+            key={year}
+            data={{ year, items }}
+            isExpandedContainer={yearsOpen.includes(year)}
+          >
+            <PublicationsContainer publications={items} year={year} />
+          </CollapsiblePubContainer>
+        ))}
       </PublicationSectionWrapper>
     </div>
   );
