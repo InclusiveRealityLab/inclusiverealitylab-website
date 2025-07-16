eaf392a51aaf405bf9b5ede52b8eec11f8e3767f
Sukhleen Kaur <skau811@aucklanduni.ac.nz>
2025-07-17 11:54:14 +1200

    feat: add loading spinner while  fetching results in the people page

--- frontend/src/pages/PeoplePage.jsx
+++ frontend/src/pages/PeoplePage.jsx
@@ -6,16 +6,20 @@ import CategoryContainer from "../components/CategoryContainer";
 import API_BASE_URL from "../sampleData/constants";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import loading from "../assets/icons/loading.svg";
 
 function PeoplePage() {
   const [people, setPeopleData] = useState([]);
+  const [isLoading, setisLoading] = useState(true);
 
   useEffect(() => {
     async function loadPeople() {
       try {
         const response = await axios.get(`${API_BASE_URL}/people`);
         const data = await response.data;
+        
         setPeopleData(data);
+        setisLoading(false);
         console.log("People data loaded:", data);
       } catch (error) {
         console.error("Error fetching people data:", error);
@@ -27,12 +31,16 @@ function PeoplePage() {
   return (
     <>
       {" "}
-      {people.length && (
+      
         <div className="mt-4 w-screen h-auto">
           <div className=" flex flex-col px-1.5 xl:px-0 justify-between items-start py-5 gap-4 w-full xl:max-w-[1032px]  xl:mx-auto">
             <h1 className="heading1 self-start">People</h1>
 
-            {peopleCategories.map((cat) => {
+            {isLoading ? (<div className="flex min-h-screen w-full items-center justify-center">
+                      <img src={loading} className="w-12 h-12" />
+                    </div>) : 
+
+            peopleCategories.map((cat) => {
               const filteredPeople = people.filter(
                 (p) => p.category === cat.name
               );
@@ -52,7 +60,7 @@ function PeoplePage() {
             {/* <PeopleCard person={people.find((p) => p.category == "lab")} /> */}
           </div>
         </div>
-      )}
+      
     </>
   );
 }
