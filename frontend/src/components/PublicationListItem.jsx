65e10cfaab263c01f2b204117325ca6664377448
Sukhleen Kaur <skau811@aucklanduni.ac.nz>
2025-07-17 11:19:59 +1200

    style: fix spacing in pub list item according to detailed spec

--- frontend/src/components/PublicationListItem.jsx
+++ frontend/src/components/PublicationListItem.jsx
@@ -1,21 +1,23 @@
 function PublicationListItem({ publication }) {
   return (
-    <div className="flex xl:flex-row-reverse flex-col justify-between items-stretch xl:w-full xl:max-w-64.5 xl:h-12.5 flex-grow basis-17 max-w-25 xl:basis-0  gap-1 border-1 border-blue-500 ">
+    <div className="flex xl:flex-row-reverse flex-col justify-between items-stretch xl:w-full xl:max-w-64.5 xl:h-12.5 flex-grow basis-17 max-w-25 xl:basis-0  xl:gap-2 gap-1 border-1 border-blue-500 ">
       <img
         src="/projectPhoto.png"
         className="xl:w-20 xl:h-12.5 w-full h-full "
       />
 
       {/* container for pub details */}
-      <div className="flex flex-col items-start pb-0.5 xl:pt-1 xl:pr-1 xl:gap-0.5  ">
-        <p className="flex bodySmall mb-0.5">
-          {publication?.conference ?? "CHI2024"}
-        </p>
+      <div className="flex flex-col items-start xl:justify-between justify-between pb-0.5 xl:pt-1 xl:pb-1 xl:gap-1.5  gap-1.5  ">
+        <div className="flex flex-col items-start xl:gap-0.5 gap-0.5">
+          <p className="flex bodySmall">
+            {publication?.conference ?? "CHI2024"}
+          </p>
 
-        <p className="bodyBigBold overflow-hidden text-ellipsis line-clamp-2  xl:mb-1 mb-1.5 border-1 border-pink-500">
-          {publication?.title ??
-            "Cymatics Cup: Shape-Changing Drinks by Leveraging Cymatics This is longer should be clamped at the end over here. Let us write more and see the result of how this works, its still showing  "}
-        </p>
+          <p className="bodyBigBold overflow-hidden text-ellipsis line-clamp-2  border-1 border-pink-500">
+            {publication?.title ??
+              "Cymatics Cup: Shape-Changing Drinks by Leveraging Cymatics This is longer should be clamped at the end over here. Let us write more and see the result of how this works, its still showing  "}
+          </p>
+        </div>
 
         {/* container for the CTA buttons : DOI and PDF links */}
         <div className="flex flex-row w-auto justify-center gap-1.5 ">
