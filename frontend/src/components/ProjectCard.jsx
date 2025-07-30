6e348ce66c326c292e14070580aba71a28d7a99d
Sukhleen Kaur <skau811@aucklanduni.ac.nz>
2025-07-30 23:10:05 +1200

    feat: add custom data type formatting and implement infor label in project card

--- frontend/src/components/ProjectCard.jsx
+++ frontend/src/components/ProjectCard.jsx
@@ -6,9 +6,18 @@ function ProjectCard({ project, className = "" }) {
       className={`${className} group flex flex-col justify-end items-end min-w-16 py-1 px-0.5 gap-1 h-20 bg-[linear-gradient(rgba(0,0,0,0.25),rgba(0,0,0,0.25)),url(/projectPhoto.png)] bg-cover`}
     >
       <h2 className="group-hover:text-text-active heading2 text-baseWhite line-clamp-2">
-        {project?.title ?? "Enhance human-food interaction in social settings "}
+        {project?.["project name"] ??
+          "Enhance human-food interaction in social settings "}
       </h2>
-      <div className="self-start flex flex-row flex-wrap gap-0.5 w-full"><InfoLabel label="Understand"/><InfoLabel label="Augment"/><InfoLabel label="Assist"/></div>
+      <div className="self-start flex flex-row flex-wrap gap-0.5 w-full">
+        {(Array.isArray(project["research theme"])
+          ? project["research theme"]
+          : [project["research theme"]]
+        ).map((theme, index) => (
+          <InfoLabel key={index} label={theme} />
+          
+        ))}
+      </div>
     </div>
   );
 }
--- frontend/src/pages/ProjectsPage.jsx
+++ frontend/src/pages/ProjectsPage.jsx
@@ -2,8 +2,30 @@ import ProjectCard from "../components/ProjectCard";
 import projects from "../sampleData/projects";
 import ProjectsContainer from "../components/ProjectsContainer";
 import PublicationSectionWrapper from "../components/wrappers/PublicationSectionWrapper";
+import { useState, useEffect } from "react";
+import loading from "../assets/icons/loading.svg";
+import axios from "axios";
+import API_BASE_URL from "../sampleData/constants";
 
 function ProjectsPage() {
+  const [projects, setProjects] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadProjects() {
+      try {
+        const reponse = await axios.get(`${API_BASE_URL}/projects`);
+        const data = await reponse.data;
+        setProjects(data);
+        console.log(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log("Error fetching projects items", error);
+      }
+    }
+    loadProjects();
+  }, []);
+
   return (
     <>
       <div className="mt-4 w-screen bg-background-white">
@@ -11,13 +33,18 @@ function ProjectsPage() {
           <h1 className="heading1 self-start">Projects</h1>
 
           <button className="heading4 mt-1">Current Projects</button>
-          <ProjectsContainer
-            projects={projects.filter((project) => project.current === true)}
-          />
-          <button className="heading4 mt-1">Past Projects</button>
-          <ProjectsContainer
-            projects={projects.filter((project) => project.current === false)}
-          />
+          {isLoading ? (
+            <div className="flex min-h-screen w-full items-center justify-center">
+              <img src={loading} className="w-12 h-12" />
+            </div>
+          ) : (
+            <ProjectsContainer
+              projects={projects.filter(
+                (project) =>
+                  project["end date"] === "" && project["type"] !== "Internal"
+              )}
+            />
+          )}
         </div>
       </div>
     </>
