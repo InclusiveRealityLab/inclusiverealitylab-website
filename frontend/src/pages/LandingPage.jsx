f4447e15f45f16cc7cd68bba6ac577ae7d05595d
Sukhleen Kaur <skau811@aucklanduni.ac.nz>
2025-08-18 21:57:51 +1200

    fix: key visual for safari browsers

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -101,10 +101,10 @@ function LandingPage() {
             type='video/webm; codecs="vp9,opus"'
           />
           {/* mp4 for Safari */}
-          {/* <source
+          <source
             src={`${import.meta.env.BASE_URL}images/keyVisual/irl_intro.mp4`}
             type='video/mp4; codecs="avc1.42E01E,mp4a.40.2"'
-          /> */}
+          />
         </video>
 
         <div className="absolute xl:left-6 xl:top-27.5 top-[240px]   max-w-[472px] min-w-[272px] xl:max-w-[592px]  z-10 flex flex-col justify-center items-start px-1.5 py-3 xl:p-5 bg-background-white gap-1.5">
