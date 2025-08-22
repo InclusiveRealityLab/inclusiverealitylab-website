98fdd0f46b1bb63994829d61cf50004d1f8b0cdf
Sukhleen Kaur <skau811@aucklanduni.ac.nz>
2025-08-22 17:18:10 +1200

    feat: add animation to the landing page as a component,  (works across devices), paragraph background can be improved

--- frontend/src/components/wrappers/PublicationSectionWrapper.jsx
+++ frontend/src/components/wrappers/PublicationSectionWrapper.jsx
@@ -1,7 +1,7 @@
 function PublicationSectionWrapper({headingContent,children}) {
   return (
     <>
-      <section className="flex flex-col bg-background-white xl:justify-items-start items-start w-screen ">
+      <section className="flex flex-col bg-background-white xl:justify-items-start items-start w-screen z-0">
         <div className="flex flex-col justify-between items-start   py-5 gap-4  w-full xl:max-w-[1032px]  mx-auto px-1.5 xl:px-0">
           <h1 className="heading1 min-w-0 whitespace-normal hyphens:auto [-webkit-hyphens:auto]" lang="en">{headingContent}</h1>
 
