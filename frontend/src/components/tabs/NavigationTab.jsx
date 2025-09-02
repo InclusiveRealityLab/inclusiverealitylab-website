65e37dc6f24682e193219e0f3161436841a374e6
Sukhleen Kaur <skau811@aucklanduni.ac.nz>
2025-09-02 16:50:53 +1200

    refactor : NavigationTab component to extend the clickable area to the background as well

--- frontend/src/components/tabs/NavigationTab.jsx
+++ frontend/src/components/tabs/NavigationTab.jsx
@@ -1,9 +1,17 @@
 import { Link } from "react-router";
 
-function NavigationTab({label,linkAddress,onClick}) {
-  return <button className="transition ease-in duration-200 hover:bg-background-secondary/40 w-9 xl:w-[120px] h-2.5" onClick={onClick} ><Link to={linkAddress} >{label} </Link>
 
-  </button>;
+function NavigationTab({ label, linkAddress, onClick }) {
+  return (
+    <Link to={linkAddress}>
+      <button
+        className="cursor-pointer transition ease-in duration-200  hover:bg-background-secondary/40 w-9 xl:w-[120px] h-2.5"
+        onClick={onClick}
+      >
+        {label}
+      </button>
+    </Link>
+  );
 }
 
 export default NavigationTab;
