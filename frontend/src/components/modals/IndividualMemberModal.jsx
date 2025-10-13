73bd9b73db27a40c2c5353bb0f5188b64f118ba3
Sukhleen Kaur <skau811@aucklanduni.ac.nz>
2025-10-14 12:57:32 +1300

    refactor: contact icons and render the contact me section conditionally

--- frontend/src/components/modals/IndividualMemberModal.jsx
+++ frontend/src/components/modals/IndividualMemberModal.jsx
@@ -2,6 +2,23 @@ import closeBlack from "../../assets/icons/closeBlack.svg";
 import ReactDOM from "react-dom";
 import Modal from "./Modal";
 import ProfilePhotoContainer from "../ProfilePhotoContainer";
+import email_blk from "../../assets/icons/email_blk.svg";
+import scholar from "../../assets/icons/scholar_blk.svg";
+import linkedin_blk from "../../assets/icons/linkedin_blk.svg";
+import facebook_blk from "../../assets/icons/fb_blk.svg";
+import x_blk from "../../assets/icons/x_blk.svg";
+
+const connectionPlatforms = [
+  { name: "Email", key: "Email", icon: email_blk },
+  { name: "Google Scholar", key: "Google Scholar", icon: scholar },
+  { name: "Linkedin", key: "Linkedin", icon: linkedin_blk },
+  { name: "Facebook", key: "Facebook", icon: facebook_blk },
+  { name: "X", key: "X", icon: x_blk },
+];
+
+const hasContactInfo = (person) => {
+  return connectionPlatforms.some((platform)=> Boolean(person[platform.key]));
+}
 
 function IndividualMemberModal({
   person,
@@ -15,15 +32,63 @@ function IndividualMemberModal({
         <Modal
           onClose={onClose}
           backgroundColor="bg-background-white"
-          horizontalGap="[48px]"
+          horizontalGap="[0px]"
         >
-          <div className="xl:w-[1008px] w-full border-black border-2 flex flex-col gap-[64px] ">
+          <div className="xl:w-[1008px] border-2 border-red my-[80px] xl:px-0 px-1.5 w-full  flex flex-col gap-[64px] ">
             <div className="w-full flex flex-col gap-[40px] items-center">
               <ProfilePhotoContainer person={person} />
               <div className="flex flex-col max-w-[640px] gap-1 items-center justify-center">
-                <p className="heading2 align-center">{`${person["Given Name"]} ${person["Family Name"]}`}</p>
-                <p className="bodyBig text-center">{ person["Affiliation"] ? person["Affiliation"] : person["Role"]} </p> 
+                <p className="heading2 text-center">{`${person["Given Name"]} ${person["Family Name"]}`}</p>
+                <p className="bodyBig text-center">
+                  {person["Affiliation"]
+                    ? person["Affiliation"]
+                    : person["Role"]}{" "}
+                </p>
               </div>
+            </div>
+            {/* Optional about me section */}
+            {person["Bio"] && (
+              <div className="flex flex-col gap-1">
+                <p className="heading4">About me</p>
+                <p className="body">{person["Bio"]}</p>
+              </div>
+            )}
+            {/* Optional areas of expertise section */}
+            {person["Expertise"] && (
+              <div className="flex flex-col gap-1">
+                <p className="heading4">Areas of expertise</p>
+                <p className="body">{person["Expertise"]}</p>
+              </div>
+            )}
+            {/* Optional website section */}
+            {person["Website"] && (
+              <div className="flex flex-col gap-1">
+                <p className="heading4">Website</p>
+                <p className="body">
+                  <a href={person["Website"]}>{person["Website"]}</a>
+                </p>
+              </div>
+            )}
+            {/* Optional contact section */}
+            <div className="flex flex-col gap-1">
+              {hasContactInfo(person) && 
+                 (
+                  <>
+                    <p className="heading4">Contact me</p>
+                    <div className="flex flex-row gap-1.5 border-2">
+                      {connectionPlatforms.map((platform) => {
+                        if (person[platform.key]) {
+                          return (
+                            <a href={person[platform.key]} key={platform.key}>
+                              <img src={platform.icon} />
+                            </a>
+                          );
+                        }
+                      })}
+                    </div>
+                  </>
+                )
+             }
             </div>
           </div>
         </Modal>
--- frontend/src/components/modals/Modal.jsx
+++ frontend/src/components/modals/Modal.jsx
@@ -9,7 +9,7 @@ function Modal({ children, onClose, type, backgroundColor, horizontalGap }) {
         className={`fixed top-0 right-0 w-full   h-screen overflow-scroll z-100 bg-black/40 `}
       >
         <div className="right-0 fixed w-full xl:max-w-68 h-screen ">
-          <div className={`${backgroundColor} overflow-y-scroll flex flex-col items-center xl:gap-${horizontalGap} gap-[56px] h-full `}>
+          <div className={`${backgroundColor} overflow-y-scroll flex flex-col items-center xl:gap-${horizontalGap} h-full `}>
           <button className="align xl:self-start self-end xl:ml-2.5 xl:mt-2.5 mt-1 mb-1 mr-1.5 cursor-pointer" onClick={onClose}>
             <img src={closeBlack}  />
           </button>
